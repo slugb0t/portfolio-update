bf341828332ccbc678546cb5e6d40076d922ab65
slugb0t <wheresdorian@gmail.com>
2021-12-02 19:59:17 -0800

    nav bar created

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'body': '#131313',
+        'selected-text-': '#A3A3FF',
+        'theme': '#663399',
+        'nav': '#404053',
+        'secondary': '#3F3F51',
+        'input-border': '#565666',
+        'input': '#2A2A35',
+      },
+      fontFamily: {
+        'poppins': ["'Poppins'", 'sans-serif'],
+        'share-tech': ["'Share-Tech-Mono'", 'sans-serif']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+}
